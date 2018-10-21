1bc3a4aed1fc805503577f6dd586bb2a8549f45c
Luis Cadena <36091745+bocasoftware@users.noreply.github.com>
2018-10-21 22:06:09 +0200

    Add files via upload

--- roman-numerals-converter.js
+++ roman-numerals-converter.js
@@ -1,8 +1,23 @@
-function telephoneCheck(str) {
-  //Regular Expressions
-   var regex = /^(1\s?)?(\(\d{3}\)|\d{3})[\s\-]?\d{3}[\s\-]?\d{4}$/;
-   return regex.test(str);
+var convertToRoman = function(num) {
+
+  var decimalValue = [ 1000, 900, 500, 400, 100, 90, 50, 40, 10, 9, 5, 4, 1 ];
+  var romanNumeral = [ 'M', 'CM', 'D', 'CD', 'C', 'XC', 'L', 'XL', 'X', 'IX', 'V', 'IV', 'I' ];
+
+  var romanized = '';
+///Cycle through decimalValue array
+  for (var index = 0; index < decimalValue.length; index++) {
+    //While index is less than the number given, push roman numeral to romanized array.
+    while (decimalValue[index] <= num) {
+      romanized += romanNumeral[index];
+      //Remove matched decimal
+      num -= decimalValue[index];
+      
+      
+    }
+  }
+
+  return romanized;
 }
 
 
-telephoneCheck("555-555-5555");
+convertToRoman(36);
